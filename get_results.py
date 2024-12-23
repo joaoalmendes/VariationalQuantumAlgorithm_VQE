217e27037b6e3e623af136ae5f34fc23e19ffb22
João Almeida Mendes <91161509+joaoalmendes@users.noreply.github.com>
2024-12-23 20:14:14 +0000

    Update get_results.py

--- get_results.py
+++ get_results.py
@@ -3,7 +3,7 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 service = QiskitRuntimeService(
     channel='ibm_quantum',
     instance='ibm-q/open/main',
-    token='1eed55196e8d2e774cb9392e0d30a1c2541e55b15be9d77518f7b9375a5516c760d48d30d9e06d3fc2233b5d2bde5518f99b13a1afdb320208337194ae616ba6'
+    token='YOUR TOKEN'
 )
 job = service.job('ct5yd5w3b8m0008bvxd0')
 job_result = job.result()
